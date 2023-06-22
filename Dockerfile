dff35d8b571d7d30ef8807f9aff59b8344e84b95
Jarsigan <jarsigan.15@cse.mrt.ac.lk>
2023-06-23 01:21:09 +0800

    Deployment checking

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM jupyter/scipy-notebook
 
 RUN mkdir my-model
-ENV MODEL_DIR=/home/jovyan/my-model
+ENV MODEL_DIR=./my-model
 ENV MODEL_FILE=svc_model.model
 
 COPY requirements.txt ./requirements.txt
@@ -13,6 +13,6 @@ COPY api.py ./api.py
 #USER 1001
 EXPOSE 8080
 
-RUN python3 train.py
+#RUN python3 train.py
 ENTRYPOINT python3 api.py
 #CMD ["python3", "api.py", "8080"]
--- api.py
+++ api.py
@@ -28,17 +28,19 @@ app = Flask(__name__)
 @app.route('/',methods=['POST', 'GET'])
 # Return predictions of inference using Iris Test Data
 def prediction():
-
+    message = 'first deployment'
     # Load and split the data
-    iris = load_iris()
-    X, y = iris.data, iris.target
-    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,shuffle=False)
+    # iris = load_iris()
+    # X, y = iris.data, iris.target
+    # X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,shuffle=False)
+    #
+    # # Classification score
+    # clf = load(MODEL_PATH)
+    # score = clf.score(X_test, y_test)
+    #
+    # return {'score': score}
 
-    # Classification score
-    clf = load(MODEL_PATH)
-    score = clf.score(X_test, y_test)
-
-    return {'score': score}
+    return message
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080) # Launch built-in we server and run this Flask webapp
